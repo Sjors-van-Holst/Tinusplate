1bc9a6ead58cdb8dcdd8c8a3c71c79008a8c3ac4
Sjors van Holst <sjorsvanholst@gmail.com>
2023-05-27 20:54:09 +0200

    ✨ Added formatter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
