09f9a8b16b516e4c9ca7439159c59b18527300d7
Sjors van Holst <sjorsvanholst@gmail.com>
2023-05-27 21:38:41 +0200

    ♻️ Added markdown parser

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { attributes } from "@/content/home.md";
+
+import { parseMarkdown } from "@/utils/markdown";
 
 export default function Home() {
-  let { title, cats } = attributes;
+  const { title } = parseMarkdown("home.md");
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>{title}</h1>
+        <h1>{ title }</h1>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
